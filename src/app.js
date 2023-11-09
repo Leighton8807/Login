2e6065dfed92183bcb478871a75981c7e2457b3d
Leighton8807 <joge.8807@gmail.com>
2023-11-08 22:44:17 -0500

    finished login, register and logout template

--- src/app.js
+++ src/app.js
@@ -43,5 +43,13 @@ app.listen(app.get('port'), ()=> {
 app.use('/', loginRoutes);
 
 app.get('/', (req, res) => {
-    res.render('home');
+
+    if (req.session.loggedin == true) {
+        
+        res.render('home', {name: req.session.name});
+
+    }else{
+        res.redirect('/login');
+    }
+    
 });
--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,9 @@ const LoginController = require('../controllers/LoginController');
 const router = express.Router();
 
 router.get('/login', LoginController.login);
+router.post('/login', LoginController.auth);
 router.get('/register', LoginController.register);
 router.post('/register', LoginController.storeUser);
+router.get('/logout', LoginController.logout);
 
 module.exports = router;
